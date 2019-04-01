834619d8fb2d1ff872ce62f848ec2af1c6b85871
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-04-01 23:41:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", () => {
   //start your code here
 });
+const lowerCaseLetters ="abcdefghijklmnopqrstuvwxyz"
+function onSubmit(event){
+  const input = document.getElementById("password-lenght")
+  console.log(input.value)
+}
+document.addEventListner
