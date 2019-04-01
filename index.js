1e0de4154fe61b5b1c7bede460d9b9435c01e206
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-04-01 23:53:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function onSubmit(event){
   const input = document.getElementById("password-lenght")
   const howManyCharacters = input.value
   for (let i= 0; i <howManyCharacters; i++) { 
-  } 
-}
+    console.log(Math.floor(Math.random()+26)
+  
+}}
 document.addEventListner('submit', onSubmit)
-
+  
