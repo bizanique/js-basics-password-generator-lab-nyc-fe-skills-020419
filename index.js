3f4da826a18510de72009dea7181e4604cd764b9
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-04-02 00:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,24 @@ function onSubmit(event){
   const howManyCharacters = input.value
   let startingString = ''
   for (let i= 0; i <howManyCharacters; i++) { 
-    const randomIndex = Math.floor(Math.random() *26)
- startingString += lowerCaseLetters[randomIndex]
+    let randomIndex = Math.floor(Math.random() * lowerCaseLetters.length)
+if (startingString.lenght< howManyCharacters){randomIndex startingString += lowerCaseLetters[randomIndex]
+ 
+  randomIndex = Math.floor(Math.random() * upperCaseLetters.length)
+ startingString += upperCaseLetters[randomIndex]
+ 
+randomIndex = Math.floor(Math.random() * numbers.length)
+ startingString += numbers[randomIndex]
+ 
+  randomIndex = Math.floor(Math.random() * specialCharacters.length)
+ startingString += specialCharacters[randomIndex]
 }
 console.log(startingString)
 const output = document.getElementById('result')
 output.innerHTML = startingString
   
 }
+const output = document getElementById('result')
+output.innerHTML = startingString
+
 document.addEventListener('submit', onSubmit)
